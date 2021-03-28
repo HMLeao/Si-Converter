41acaf38364dc58df7354fe737eee863a4a8eb61
Henrique Martins <leaomartinshenrique@gmail.com>
2021-03-28 03:04:44 -0300

    initial commit

---
+++ src/main/java/com/compnany/unit_converter/si/unit/converter/SiUnitConverterApplication.java
@@ -0,0 +1,13 @@
+package com.compnany.unit_converter.si.unit.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiUnitConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiUnitConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compnany/unit_converter/si/unit/converter/SiUnitConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compnany.unit_converter.si.unit.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiUnitConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
